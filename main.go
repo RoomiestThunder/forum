6c2371cc5f83743dc5a1162ddb4a8a1b8adafbdd
stursynbay <roomiestthunder@gmail.com>
2025-07-19 21:34:55 +0500

    commit #2, made template

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 )
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-
+	templ, _ := template.ParseFiles("templates/home_page.html")
+	templ.Execute(w, nil)
 }
 
 func main() {
